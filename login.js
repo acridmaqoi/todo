a2da7d361621236fac3b8425ec0ef13a7c033ffe
acridmaqoi <65739619+acridmaqoi@users.noreply.github.com>
2021-01-31 18:50:29 +0000

    First commit

---
+++ login.js
@@ -0,0 +1,16 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+    
+    if (username === "admin" && password === "admin") {
+        alert("登录成功");
+        location.reload();
+    } else {
+        alert("用户名或密码错误");
+    }
+})
