1ba0646c5765434a29328cf117ffd84d5ba90d13
acridmaqoi <65739619+acridmaqoi@users.noreply.github.com>
2021-02-27 13:53:15 +0000

    Added register page

--- main/list.php
+++ main/list.php
@@ -1,3 +1,13 @@
+<?php
+// We need to use sessions, so you should always start sessions using the below code.
+session_start();
+// If the user is not logged in redirect to the login page...
+if (!isset($_SESSION['loggedin'])) {
+	header('Location: ../index.html');
+	exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +18,8 @@
 </head>
 <body>
     <h1>清单</h1>
+    <a href="logout.php">登出</a>
+
     <input type="text" id="myInput" placeholder="标题...">
     <button onclick="newElement()" class="addBtn">加</button>
 
